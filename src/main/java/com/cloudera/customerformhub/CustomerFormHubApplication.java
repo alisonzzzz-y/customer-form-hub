dcccaa90a8c16befa8d4d212231bbea4a9daff40
Alison Zhang <alison.zhangyan@gmail.com>
2026-06-23 14:36:33 +0100

    Initial commit: Spring Boot project connected to MySQL

---
+++ src/main/java/com/cloudera/customerformhub/CustomerFormHubApplication.java
@@ -0,0 +1,13 @@
+package com.cloudera.customerformhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerFormHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerFormHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudera/customerformhub/CustomerFormHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudera.customerformhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerFormHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
